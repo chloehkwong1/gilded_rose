6c2ed64ad0ba56bf1d9451785341016b4a70921a
Chloe <chloe.wong@madetech.com>
2021-01-29 17:01:16 +0000

    using pytest parametrize

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -1,14 +1,20 @@
 # -*- coding: utf-8 -*-
-
+import pytest
 from gilded_rose import Item, GildedRose
 
+@pytest.mark.parametrize("name, sell_in, quality", [
+    ("+5 Dexterity Vest", 10, 20)
+])
 
-class TestGildedRose():
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
-        assert "foo" == items[0].name
 
-    def test_
+def test_foo(name, sell_in, quality):
+    items = [Item(name, sell_in, quality)]
+    gilded_rose = GildedRose(items)
+    gilded_rose.update_quality()
+    assert "+5 Dexterity Vest" == items[0].name
+
+def test_quality_degrades_twice_as_fast_passed_sell_by_date():
+    pass
+
+
 
