e45fe715af915f3e28050699a47ef9f0c4eaecad
Chloe <chloe.wong@madetech.com>
2021-01-30 15:45:19 +0000

    Modified tests to add in an extra case for two days after sellin date

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -13,8 +13,32 @@ def test_foo(name, sell_in, quality):
     gilded_rose.update_quality()
     assert "+5 Dexterity Vest" == items[0].name
 
-def test_quality_degrades_twice_as_fast_passed_sell_by_date():
-    pass
+@pytest.mark.parametrize("name, sell_in, quality, expected_value",
+ [
+    ("testitem1", -1, 20, 18),
+    ("testitem2", -1, 15, 13)
+])
+
+def test_quality_degrades_twice_as_fast_one_day_after_sellin_date(name, sell_in, quality, expected_value):
+    items = [Item(name, sell_in, quality)]
+    gilded_rose = GildedRose(items)
+    gilded_rose.update_quality()
+    assert items[0].quality == expected_value
+
+@pytest.mark.parametrize("name, sell_in, quality, expected_value",
+ [
+    ("testitem1", -2, 20, 16),
+    ("testitem2", -2, 15, 11)
+])
+
+
+def test_quality_degrades_twice_as_fast_two_days_after_sellin_date(name, sell_in, quality, expected_value):
+    items = [Item(name, sell_in, quality)]
+    gilded_rose = GildedRose(items)
+    gilded_rose.update_quality()
+    gilded_rose.update_quality()
+    assert items[0].quality == expected_value
+
 
 
 
