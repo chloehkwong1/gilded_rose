fbcf2b7eff8687d4ceb74cf66cb350cb87bcda9b
Chloe <chloe.wong@madetech.com>
2021-01-30 15:55:49 +0000

    Deleted test_foo

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -2,17 +2,6 @@
 import pytest
 from gilded_rose import Item, GildedRose
 
-@pytest.mark.parametrize("name, sell_in, quality", [
-    ("+5 Dexterity Vest", 10, 20)
-])
-
-
-def test_foo(name, sell_in, quality):
-    items = [Item(name, sell_in, quality)]
-    gilded_rose = GildedRose(items)
-    gilded_rose.update_quality()
-    assert "+5 Dexterity Vest" == items[0].name
-
 @pytest.mark.parametrize("name, sell_in, quality, expected_value",
  [
     ("testitem1", -1, 20, 18),
