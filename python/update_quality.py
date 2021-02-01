833f158182a6ad6a39e8d15869fcc9f92449afaa
Chloe <chloe.wong@madetech.com>
2021-02-01 14:11:49 +0000

    edited update quality methods to include that for after sellin date too. Added a update_normal_quality method

--- python/update_quality.py
+++
@@ -1,6 +0,0 @@
-def increase_quality(item):
-    item.quality = item.quality + 1
-
-
-def decrease_quality(item):
-    item.quality = item.quality - 1
---
+++ python/update_quality_items.py
@@ -0,0 +1,22 @@
+def update_backstage_passes_quality(item):
+    item.quality = item.quality + 1
+    if item.sell_in < 6:
+        item.quality = item.quality + 1
+        item.quality = item.quality + 1
+    else:
+        item.quality = item.quality + 1
+
+    if item.sell_in < 0:
+        item.quality = 0
+
+def update_brie_quality(item):
+    item.quality = item.quality + 1
+
+    if item.sell_in < 0:
+        item.quality = item.quality + 1
+
+def update_normal_quality(item):
+    item.quality = item.quality - 1
+
+    if item.sell_in < 0:
+        item.quality = item.quality - 1
